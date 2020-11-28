cb001c28e5ea1acd6db51009995d28319d416338
chenxiao <you@example.com>
2020-11-28 19:02:42 +0800

    meituan-app

---
+++ src/components/products/mapFav.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <el-row>
+      <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1606564827246&di=0892ae5f14f670336aeaaa27cdd7a78c&imgtype=0&src=http%3A%2F%2Fimgs.soufunimg.com%2Fnews%2F2011_03%2F22%2Fhouse%2F1300805030252_000.jpg" alt="">
+    </el-row>
+  </div>
+</template>
+<style scoped>
+div img{
+  margin-left: 10%;
+  width: 90%;
+}
+</style>
